d4c22786facc1081cbe0f4bd6fa72da394b2212f
ssybh2 <ssybh2@nottingham.edu.cn>
2026-07-05 23:16:17 +0800

    Add uORB subscriptions and debug printing

--- l1_adaptive_control/L1AdaptiveControl.cpp
+++ l1_adaptive_control/L1AdaptiveControl.cpp
@@ -16,9 +16,8 @@ bool L1AdaptiveControl::init()
 {
 PX4_INFO("L1 adaptive control init");
 
-// 10 Hz for the first skeleton test.
-// Later, after uORB subscriptions and controller logic are added,
-// this can be changed to callback-driven or higher-rate scheduling.
+// 10 Hz debug loop.
+// At this stage we only subscribe and print uORB states.
 ScheduleOnInterval(100_ms);
 
 return true;
@@ -35,14 +34,90 @@ return;
 perf_begin(_loop_perf);
 perf_count(_loop_interval_perf);
 
+if (_vehicle_local_position_sub.update(&_vehicle_local_position)) {
+_has_local_position = true;
+}
+
+if (_vehicle_attitude_sub.update(&_vehicle_attitude)) {
+_has_attitude = true;
+}
+
+if (_vehicle_angular_velocity_sub.update(&_vehicle_angular_velocity)) {
+_has_angular_velocity = true;
+}
+
+if (_vehicle_status_sub.update(&_vehicle_status)) {
+_has_vehicle_status = true;
+}
+
 const hrt_abstime now_us = hrt_absolute_time();
 
 if (now_us - _last_print_us > 1000000) {
-PX4_INFO("L1 adaptive control running");
+print_debug_info();
 _last_print_us = now_us;
 }
 
 perf_end(_loop_perf);
+}
+
+void L1AdaptiveControl::print_debug_info()
+{
+PX4_INFO("========== L1 adaptive uORB debug ==========");
+
+if (_has_vehicle_status) {
+const bool armed = (_vehicle_status.arming_state == vehicle_status_s::ARMING_STATE_ARMED);
+
+PX4_INFO("status: armed=%d arming_state=%u nav_state=%u failsafe=%d",
+ (int)armed,
+ (unsigned)_vehicle_status.arming_state,
+ (unsigned)_vehicle_status.nav_state,
+ (int)_vehicle_status.failsafe);
+
+} else {
+PX4_WARN("vehicle_status: no data yet");
+}
+
+if (_has_local_position) {
+PX4_INFO("local_position valid: xy=%d z=%d v_xy=%d v_z=%d",
+ (int)_vehicle_local_position.xy_valid,
+ (int)_vehicle_local_position.z_valid,
+ (int)_vehicle_local_position.v_xy_valid,
+ (int)_vehicle_local_position.v_z_valid);
+
+PX4_INFO("pos NED [m]: x=%.3f y=%.3f z=%.3f",
+ (double)_vehicle_local_position.x,
+ (double)_vehicle_local_position.y,
+ (double)_vehicle_local_position.z);
+
+PX4_INFO("vel NED [m/s]: vx=%.3f vy=%.3f vz=%.3f",
+ (double)_vehicle_local_position.vx,
+ (double)_vehicle_local_position.vy,
+ (double)_vehicle_local_position.vz);
+
+} else {
+PX4_WARN("vehicle_local_position: no data yet");
+}
+
+if (_has_attitude) {
+PX4_INFO("attitude q body_FRD_to_NED: w=%.4f x=%.4f y=%.4f z=%.4f",
+ (double)_vehicle_attitude.q[0],
+ (double)_vehicle_attitude.q[1],
+ (double)_vehicle_attitude.q[2],
+ (double)_vehicle_attitude.q[3]);
+
+} else {
+PX4_WARN("vehicle_attitude: no data yet");
+}
+
+if (_has_angular_velocity) {
+PX4_INFO("angular_velocity body FRD [rad/s]: p=%.4f q=%.4f r=%.4f",
+ (double)_vehicle_angular_velocity.xyz[0],
+ (double)_vehicle_angular_velocity.xyz[1],
+ (double)_vehicle_angular_velocity.xyz[2]);
+
+} else {
+PX4_WARN("vehicle_angular_velocity: no data yet");
+}
 }
 
 int L1AdaptiveControl::task_spawn(int argc, char *argv[])
@@ -72,6 +147,12 @@ int L1AdaptiveControl::print_status()
 {
 PX4_INFO("L1 adaptive control is running");
 
+PX4_INFO("has local_position=%d attitude=%d angular_velocity=%d vehicle_status=%d",
+ (int)_has_local_position,
+ (int)_has_attitude,
+ (int)_has_angular_velocity,
+ (int)_has_vehicle_status);
+
 perf_print_counter(_loop_perf);
 perf_print_counter(_loop_interval_perf);
 
@@ -94,8 +175,13 @@ R"DESCR_STR(
 ### Description
 L1 adaptive control module skeleton for PX4 v1.17.0.
 
-This first version only starts, schedules itself, and prints a heartbeat message.
-No control output is published yet.
+Current stage:
+- Subscribe vehicle_local_position
+- Subscribe vehicle_attitude
+- Subscribe vehicle_angular_velocity
+- Subscribe vehicle_status
+- Print debug state only
+- No control output is published
 )DESCR_STR");
 
 PRINT_MODULE_USAGE_NAME("l1_adaptive_control", "controller");
--- l1_adaptive_control/L1AdaptiveControl.hpp
+++ l1_adaptive_control/L1AdaptiveControl.hpp
@@ -9,6 +9,12 @@
 #include <drivers/drv_hrt.h>
 #include <lib/perf/perf_counter.h>
 
+#include <uORB/Subscription.hpp>
+#include <uORB/topics/vehicle_local_position.h>
+#include <uORB/topics/vehicle_attitude.h>
+#include <uORB/topics/vehicle_angular_velocity.h>
+#include <uORB/topics/vehicle_status.h>
+
 using namespace time_literals;
 
 class L1AdaptiveControl :
@@ -31,6 +37,27 @@ int print_status() override;
 private:
 void Run() override;
 
+void print_debug_info();
+
+// uORB subscriptions
+uORB::Subscription _vehicle_local_position_sub{ORB_ID(vehicle_local_position)};
+uORB::Subscription _vehicle_attitude_sub{ORB_ID(vehicle_attitude)};
+uORB::Subscription _vehicle_angular_velocity_sub{ORB_ID(vehicle_angular_velocity)};
+uORB::Subscription _vehicle_status_sub{ORB_ID(vehicle_status)};
+
+// Latest copied uORB data
+vehicle_local_position_s _vehicle_local_position{};
+vehicle_attitude_s _vehicle_attitude{};
+vehicle_angular_velocity_s _vehicle_angular_velocity{};
+vehicle_status_s _vehicle_status{};
+
+// Data availability flags
+bool _has_local_position{false};
+bool _has_attitude{false};
+bool _has_angular_velocity{false};
+bool _has_vehicle_status{false};
+
+// Performance counters
 perf_counter_t _loop_perf{perf_alloc(PC_ELAPSED, MODULE_NAME": cycle")};
 perf_counter_t _loop_interval_perf{perf_alloc(PC_INTERVAL, MODULE_NAME": interval")};
 
